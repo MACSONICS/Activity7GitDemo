ecf538f80cabed8cd66e45ce3f4d0c9354e9491e
markson aigbodi <maigbodi@firebrandtraining.com>
2025-10-28 14:12:17 +0000

    my first commit

---
+++ src/main/java/com/bpp/MyApplication.java
@@ -0,0 +1,17 @@
+package com.bpp;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class MyApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MyApplication.class);
+    }
+
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(MyApplication.class, args);
+    }
+}
